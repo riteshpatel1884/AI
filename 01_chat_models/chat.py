3aa6f6515e13215dd4333a3aa897837d8cd4f6b6
Ritesh Patel <150593440+riteshpatel1884@users.noreply.github.com>
2026-09-23 23:59:38 +0530

    Comment out chat model initialization code
    Comment out model initialization and response printing.

--- 01_chat_models/chat.py
+++ 01_chat_models/chat.py
@@ -3,7 +3,7 @@
 # from langchain.chat_models import init_chat_model
 # load_dotenv() 
 
-# model = init_chat_model("google_genai:gemini-2.5-flash-lite")
+# model = init_chat_model("google_genai:gemini-2.5-f
 
 # response = model.invoke("What is Cricket?")
 # print(response.content)
